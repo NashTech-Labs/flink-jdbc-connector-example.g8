63ec9a383ca18baf7188ea6dde22d21eec80aa83
Prabhat Kashyap <prabhat.kashyap@knoldus.in>
2017-03-28 15:35:12 +0530

    Added Sink

--- src/main/java/com/knoldus/FlinkJDBCExample.java
+++ src/main/java/com/knoldus/FlinkJDBCExample.java
@@ -13,27 +13,36 @@ import org.apache.flink.types.Row;
 
 public class FlinkJDBCExample {
 
+  /**
+   * In this example Flink will read from the table `data` present in Database`source` and will
+   * write in `mydata` table present in Database `sink`. We are using MySQL database for this
+   * example.
+   * Pre-requiste:
+   * Two Database: Source and Sink
+   * `Source` with table `data` from which we have to copy data.
+   * `Sink` with table `mydata` in which we have to copy data.
+   */
   public static void main(String[] args) throws Exception {
 
     final TypeInformation<?>[] fieldTypes =
         new TypeInformation<?>[] { BasicTypeInfo.INT_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO };
-
 
     final RowTypeInfo rowTypeInfo = new RowTypeInfo(fieldTypes);
 
     //Creates an execution environment that represents the context in which the program is currently executed.
     ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
 
-
     String selectQuery = "select * from Student";
     String driverName = "com.mysql.cj.jdbc.Driver";
-    String dbURL = "jdbc:mysql://localhost:3306/test";
+    String sourceDB = "source";
+    String sinkDB = "sink";
+    String dbURL = "jdbc:mysql://localhost:3306/";
     String dbPassword = "root";
     String dbUser = "root";
 
     //Define Input Format Builder
     JDBCInputFormat.JDBCInputFormatBuilder inputBuilder =
-        JDBCInputFormat.buildJDBCInputFormat().setDrivername(driverName).setDBUrl(dbURL)
+        JDBCInputFormat.buildJDBCInputFormat().setDrivername(driverName).setDBUrl(dbURL + sourceDB)
             .setQuery(selectQuery).setRowTypeInfo(rowTypeInfo).setUsername(dbUser)
             .setPassword(dbPassword);
 
@@ -50,9 +59,9 @@ public class FlinkJDBCExample {
     transformedSet.print();
 
     //Define Output Format Builder
-    String insertQuery = "INSERT INTO Student (id, name) values (?, ?)";
+    String insertQuery = "INSERT INTO mydata (id, name) values (?, ?)";
     JDBCOutputFormat.JDBCOutputFormatBuilder outputBuilder =
-        JDBCOutputFormat.buildJDBCOutputFormat().setDrivername(driverName).setDBUrl(dbURL)
+        JDBCOutputFormat.buildJDBCOutputFormat().setDrivername(driverName).setDBUrl(dbURL + sinkDB)
             .setQuery(insertQuery).setSqlTypes(new int[] { Types.INTEGER, Types.VARCHAR })
             .setUsername(dbUser).setPassword(dbPassword);
 
